827e1c6aea8167243b02221ed7a662033d31b942
JP-1997 <jyotiprakash.jpl.99@gmail.com>
2020-07-30 18:08:45 +0530

    initial commit

---
+++ src/main/java/com/jp/learnahobby/LearnahobbyApplication.java
@@ -0,0 +1,13 @@
+package com.jp.learnahobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnahobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnahobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/learnahobby/LearnahobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.learnahobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnahobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
